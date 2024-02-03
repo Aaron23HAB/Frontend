17456b04a32f19815e9b016ef3c30f20af5aa65d
Aaron23HAB <drummer.eiron@gmail.com>
2024-02-03 18:41:13 +0100

    cosillas

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,18 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const LoginPage = () => {
   console.log(`pepe`);
   return (
     <section>
       <h1> Login</h1>
-      <p>aqui</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quo ad
-        sunt corporis beatae voluptate quasi consectetur doloremque neque sed,
-        perferendis consequatur reprehenderit eius ducimus aut laudantium!
-        Accusantium, culpa non. Minus enim tenetur sunt doloremque dolor
-        veritatis corrupti ab vel voluptatem esse cum, porro libero eius dolorum
-        iure obcaecati ullam laudantium inventore accusamus possimus! Aliquid,
-        doloribus? Ullam a temporibus vitae!
-      </p>
+      <li>
+        <Link to={"login"}>Login</Link>
+      </li>
     </section>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import { Auth } from "./Auth"
+import { Auth } from "./Auth";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-      <header>
-        <h1> App de notas </h1>
-        <nav>
+  return (
+    <header>
+      <h1>
+        <Link to={"/"}>App de Notas</Link>
+      </h1>
+      <nav>
         <Auth />
       </nav>
-      </header>
-    );
-}
+    </header>
+  );
+};
